e882a9f4f16e533bad31bc1747f2175ad6d61e8f
specificuser1 <motorolag14rx@gmail.com>
2026-03-19 18:57:41 +0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,24 @@
 import sqlite3
 
-class DB:
+class Database:
     def __init__(self):
         self.conn = sqlite3.connect("bots.db", check_same_thread=False)
-        self.conn.execute("CREATE TABLE IF NOT EXISTS bots(token TEXT)")
+        self.conn.execute("""
+            CREATE TABLE IF NOT EXISTS bots(
+                token TEXT UNIQUE
+            )
+        """)
         self.conn.commit()
 
-    def add_bot(self, token):
-        self.conn.execute("INSERT INTO bots VALUES(?)", (token,))
+    def add_bot(self, token: str):
+        self.conn.execute("INSERT OR IGNORE INTO bots (token) VALUES (?)", (token,))
         self.conn.commit()
 
-    def get_bots(self):
-        data = self.conn.execute("SELECT token FROM bots").fetchall()
-        return [i[0] for i in data]
+    def add_bulk(self, tokens: list[str]):
+        for t in tokens:
+            self.conn.execute("INSERT OR IGNORE INTO bots (token) VALUES (?)", (t,))
+        self.conn.commit()
+
+    def get_all(self):
+        result = self.conn.execute("SELECT token FROM bots").fetchall()
+        return [i[0] for i in result]
