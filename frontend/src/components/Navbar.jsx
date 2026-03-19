ed80d29799a734df791e3e96d1acb4d787103331
specificuser1 <motorolag14rx@gmail.com>
2026-03-19 19:12:55 +0500

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="px-6 py-4 bg-blue-600 text-white shadow-md flex items-center justify-between">
+      <h1 className="text-xl font-bold">Webot Hosting</h1>
+    </div>
+  );
+}
